3cb9be2c470e3185e3320346afd8f6cf58e2e58b
Jack <2874766l@student.gla.ac.uk>
2025-10-25 14:44:25 +0100

    database start

---
+++ backend/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from database import base ## still to add base
+
+class Room(base):
+
+
+
+class Bug(base):

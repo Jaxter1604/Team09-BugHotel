afd41010e5303461b3dcde24059495d6983cd78c
Jack <2874766l@student.gla.ac.uk>
2025-10-25 14:53:16 +0100

    delete un-needed

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
-from sqlalchemy.orm import relationship
-from database import base ## still to add base
-
-class Room(base):
-
-
-
-class Bug(base):

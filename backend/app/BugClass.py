e3feb41cc983457b1ee43c604c98c4ec1295333e
Alice Thomson <2884278t@student.gla.ac.uk>
2025-10-25 15:22:05 +0100

    fixed bugclass and added roomclass

--- backend/app/BugClass.py
+++ backend/app/BugClass.py
@@ -15,12 +15,11 @@ def createBug(bugName):
 
 
 class Bug:
-    def __init__(species: str, budget: int, preyOrPreditor, agression: int, canFly: bool, agression: int, visibility: int, preferredEnvironment, sprite_path: str, size: int):
+    def __init__(species: str, budget: int, preyOrPreditor, agression: int, canFly: bool, preferredEnvironment, sprite_path: str, size: int):
         self.species = species
         self.preyOrPreditor = preyOrPreditor
         self.agression = agression
         self.canFly = canFly
-        self.visibility = visibility
         self.sprite_path = sprite_path
 
 
